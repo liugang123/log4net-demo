030955776dafd53f79f28d6790032042970ef34d
liugang19 <liugang19@JT-247711>
2021-03-09 17:28:19 +0800

    update 0309

---
+++ com.example.common/LogHelper.cs
@@ -0,0 +1,47 @@
+﻿using log4net;
+using System;
+
+namespace com.example.common
+{
+    public class LogHelper
+    {
+        public static void Debug(string message)
+        {
+            WriteLog(LogLevelEnum.Debug, message);
+        }
+
+        public static void Info(string message)
+        {
+            WriteLog(LogLevelEnum.Info, message);
+        }
+
+        public static void Warn(string message)
+        {
+            WriteLog(LogLevelEnum.Warn, message);
+        }
+
+        public static void Error(string message, Exception e)
+        {
+            WriteLog(LogLevelEnum.Error, message, e);
+        }
+
+        private static void WriteLog(LogLevelEnum logLevel, string message, Exception e = null)
+        {
+            switch (logLevel)
+            {
+                case LogLevelEnum.Debug:
+                    LogManager.GetLogger("logDebug").Debug(message);
+                    break;
+                case LogLevelEnum.Info:
+                    LogManager.GetLogger("logInfo").Info(message);
+                    break;
+                case LogLevelEnum.Warn:
+                    LogManager.GetLogger("logWarn").Warn(message);
+                    break;
+                case LogLevelEnum.Error:
+                    LogManager.GetLogger("logError").Error(message, e);
+                    break;
+            }
+        }
+    }
+}
---
+++ com.example.host/Program.cs
@@ -0,0 +1,29 @@
+﻿using com.example.common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace com.example.host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("start");
+
+           // log4net.Config.XmlConfigurator.Configure();
+
+            LogHelper.Debug("Debug");
+            LogHelper.Info("Info");
+            LogHelper.Warn("Warn");
+            LogHelper.Error("Error", new Exception("exception throw"));
+
+            Console.WriteLine("end");
+            Console.ReadLine();
+        }
+    }
+}
